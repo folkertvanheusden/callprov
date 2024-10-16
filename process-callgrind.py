1203167055cb4dc6a978f3e2ab0c7a7dc52575bf
Folkert van Heusden <mail@vanheusden.com>
2024-10-16 16:45:46 +0200

    html output

--- process-callgrind.py
+++ process-callgrind.py
@@ -1,6 +1,40 @@
 #! /usr/bin/python3
 
+import getopt
 import sys
+
+def cmdline_help():
+    print('-i x   callgrind output file to process')
+    print('-f     filter output')
+    print('-h     this output')
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:fh')
+except getopt.GetoptError as err:
+    print(err)
+    cmdline_help()
+    sys.exit(2)
+
+input_file = None
+filter_output = False
+
+for o, a in opts:
+    if o == '-i':
+        input_file = a
+    elif o == '-f':
+        filter_output = True
+    elif o == '-h':
+        cmdline_help()
+        sys.exit(0)
+    else:
+        cmdline_help()
+        sys.exit(1)
+
+if input_file == None:
+    print('No file selected')
+    print()
+    cmdline_help()
+    sys.exit(1)
 
 data = dict()
 mcnt = 0
@@ -10,21 +44,24 @@ cfile = None
 func = None
 
 lnr = 0
-fh = open(sys.argv[1], 'r')
+fh = open(input_file, 'r')
 for line in fh.readlines():
     lnr += 1
     line = line.strip()
     if line == '':
         continue
     if line[0:3] == 'fl=':
-        file = line[3:]
+        cfile = file = line[3:]
     elif line[0:4] == 'cfl=' or line[0:4] == 'cfi=':
         cfile = line[4:]
     elif line[0:3] == 'fn=':
         func = line[3:]
-    elif line[0:4] == 'cfn=':
-        func = line[4:]
+#    elif line[0:4] == 'cfn=':
+#        func = line[4:]
     elif line[0] >= '0' and line[0] <= '9':
+        if func[0:2] == '0x':
+            continue
+
         parts = line.split()
         ln = int(parts[0])
         cost = int(parts[1])
@@ -52,8 +89,33 @@ for file in data:
     except FileNotFoundError as e:
         pass
 
+print('<html>')
+print('<body>')
+
 for file in data:
+    header_emitted = False
     for func in data[file]:
         if func != ' contents ':
-            for ln in data[file][func]:
-                print(f'{file} {func} {ln} {data[file][" contents "][ln] if " contents " in data[file] and ln < len(data[file][" contents "]) else ""}')
+            suppress = False
+            if " contents " in data[file]:
+                for ln in data[file][func]:
+                    if ln > len(data[file][" contents "]):
+                        suppress = True
+                        break
+            else:
+                suppress = True
+
+            if suppress == False or filter_output == False:
+                if header_emitted == False:
+                    print(f'<h2>{file}</h2>')
+                    header_emitted = True
+
+                print(f'<h3>{func}</h3>')
+                print(f'<table><tr><th>line number</th><th>const</th><th>contents</th></tr>')
+                for ln in data[file][func]:
+                    use_nr = ln - 1
+                    print(f'<tr><td>{ln}</td><td>{data[file][func][ln]}</td><td>{data[file][" contents "][use_nr] if " contents " in data[file] and use_nr < len(data[file][" contents "]) else ""}</td></tr>')
+                print('</table>')
+
+print('</body>')
+print('</html>')
